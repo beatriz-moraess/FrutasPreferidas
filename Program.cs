9c7049189c74c32be5570328ed3c011da391e385
beatriz giovanna <beatriz.giovanna@gmail.com>
2025-02-24 11:16:15 -0300

    Finaliza Programa

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("---------------");
+Console.WriteLine("--- Frutas Favoritas ---");
+Console.WriteLine("---------------");
+
+Console.WriteLine();
+
+Console.Write("1. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Morango");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("2. ");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("uva");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("3. ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("pitaya");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("4. ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("banana");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("5. ");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Kiwi");
+Console.ResetColor();
