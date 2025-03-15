65dca1626f47fd106a5d0265a0ffd48160b2f4aa
Beatriz <beatriz.giovanna@gmail.com>
2025-03-15 19:24:18 -0300

    finaliza programa

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("---------------");
 Console.WriteLine();
 
 Console.Write("1. ");
-Console.ForegroundColor = ConsoleColor.Red;
+Console.ForegroundColor = ConsoleColor.DarkRed;
 Console.WriteLine("Morango");
 Console.ResetColor();
 
@@ -15,26 +15,26 @@ Console.WriteLine();
 
 Console.Write("2. ");
 Console.ForegroundColor = ConsoleColor.DarkMagenta;
-Console.WriteLine("uva");
+Console.WriteLine("Uva");
 Console.ResetColor();
 
 Console.WriteLine();
 
 Console.Write("3. ");
-Console.ForegroundColor = ConsoleColor.Magenta;
-Console.WriteLine("pitaya");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.WriteLine("Pitaya");
 Console.ResetColor();
 
 Console.WriteLine();
 
 Console.Write("4. ");
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("banana");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Maçã verde");
 Console.ResetColor();
 
 Console.WriteLine();
 
 Console.Write("5. ");
-Console.ForegroundColor = ConsoleColor.DarkGreen;
-Console.WriteLine("Kiwi");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("Abacaxi");
 Console.ResetColor();
